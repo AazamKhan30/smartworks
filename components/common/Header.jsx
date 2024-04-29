6d8ffc6654555247fc6c73eacce9501abf7abf06
Aazam Khan <azam.khan323@gmail.com>
2024-04-29 12:38:32 +0530

    move navbar content

--- components/common/Header.jsx
+++ components/common/Header.jsx
@@ -5,9 +5,132 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import { MdOutlineSearch } from "react-icons/md";
-import { navItems } from '@/content';
 import Link from 'next/link';
 import Image from 'next/image';
+
+export const navItems = [
+  {
+    label: "Explore Locations",
+    url: "#",
+    children: [
+        {
+          label: "Delhi-NCR",
+          link: "delhi-ncr/",
+      
+        },
+        {
+          label: "Bangalore",
+          link: "bangalore",
+       
+        },
+  
+        {
+          label: "Mumbai",
+          link: "mumbai",
+   
+        },
+        {
+          label: "Pune",
+          link: "pune",
+  
+        },
+        {
+            label: "Chennai",
+            link: "chennai",
+    
+        },
+        {
+            label: "Hyderabad",
+            link: "hyderabad",
+    
+        },
+        {
+            label: "Kolkata",
+            link: "kolkata",
+    
+        },
+        {
+            label: "Jaipur",
+            link: "jaipur",
+    
+        },
+        {
+            label: "Indore",
+            link: "indore",
+    
+        },
+        {
+            label: "Ahmedabad",
+            link: "ahmedabad",
+    
+        },
+
+        {
+            label: "Kochi",
+            link: "kochi",
+    
+        },
+
+        {
+            label: "Noida",
+            link: "noida",
+    
+        },
+
+        {
+            label: "Dehi",
+            link: "delhi",
+    
+        },
+
+        
+        {
+            label: "Gurgaon",
+            link: "gurgaon",
+    
+        },
+      ]
+  },
+  {
+    label: "Enterprise",
+    url: "#",
+  },
+  {
+    label: "Resources",
+    url: "#",
+    children: [
+      {
+        label: "Blog",
+        link: "#",
+    
+      },
+      {
+        label: "Newsroom",
+        link: "#",
+     
+      },
+
+      {
+        label: "Insights",
+        link: "#",
+ 
+      },
+      {
+        label: "Careers",
+        link: "#",
+
+      },
+    ],
+  },
+  {
+    label: "About",
+    url: "#",
+  },
+  {
+    label: "Contact",
+    url: "#",
+  },
+];
 
 
 const Header = () => {
